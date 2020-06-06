afa4b989aeafc4085c46d2d7c5fce809158f6561
Eduardo Vizcaino <evigra@hotmail.com>
2020-06-05 20:39:02 -0500

    Update

--- modulos/webHome/index.php
+++ modulos/webHome/index.php
@@ -5,7 +5,7 @@
 	$objeto->words["companyHome"]                  ="Aun en las circunstancias adversas hay que tener esperanza y fe. FE en lo que somos capaces de hacer y ESPERANZA en que llegado el momento lo haremos sin titubear";
 	$objeto->words["companyMision"]                ="Proporcionar diferentes alternativas y un nuevo concepto  de capacitación en seguridad e higiene industrial, para satisfacer las diferentes necesidades de nuestros clientes, basados en la mejora continua para obtener lealtad en nuestros clientes.";
 	$objeto->words["companyVision"]                ="Ser lideres en la capacitación y consultoría en seguridad industrial y medio ambiente";
-	$objeto->words["companyContact"]               ="<b>LLamanos Aqui </b><br>312 129 0333<br>314 352 0972<br><br><b>Escribenos Aqui</b>contacto@raul.com<br>";
+	$objeto->words["companyContact"]               ="<b>LLamanos Aqui </b><br>312 129 0333<br>314 352 0972<br><br><b>Escribenos Aqui</b>contacto@seguridadsisi.com <br>";
 	
 	$objeto->words["system_module"]             =$objeto->__VIEW_SHOW();
 	$objeto->words                              =$objeto->__INPUT($objeto->words,$objeto->sys_fields);      
